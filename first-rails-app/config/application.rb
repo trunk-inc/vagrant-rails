69b89f081c08cfa14c28bab7b19c88c7fc3f6b9e
川崎暑士 <kawasaki8910@gmail.com>
2019-06-08 13:28:56 +0900

    fix compile

--- first-rails-app/config/application.rb
+++ first-rails-app/config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -29,5 +29,6 @@ module VagrantRails
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.serve_static_assets = true
   end
 end
